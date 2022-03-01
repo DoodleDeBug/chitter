061f7ce565a97fbba4ac7afe440112f614ba6302
M <75613073+DoodleDeBug@users.noreply.github.com>
2022-03-01 13:54:06 +0000

    create user model

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root "articles#index"
 
   get "/articles", to: "articles#index"
+
+  get "/signup", to: "signup#index"
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
